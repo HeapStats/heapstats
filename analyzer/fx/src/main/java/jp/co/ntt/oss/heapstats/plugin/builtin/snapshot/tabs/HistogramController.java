d3d67e1c117f55392e2b9555a77472dbf369cb9c
Yasumasa Suenaga <yasuenag@gmail.com>
2016-10-13 18:44:30 +0900

    Add parameter for TickUnit on X axis

--- analyzer/fx/src/main/java/jp/co/ntt/oss/heapstats/plugin/builtin/snapshot/tabs/HistogramController.java
+++ analyzer/fx/src/main/java/jp/co/ntt/oss/heapstats/plugin/builtin/snapshot/tabs/HistogramController.java
@@ -269,7 +269,7 @@ public class HistogramController implements Initializable {
         long startEpoch = currentTarget.get().get(0).getSnapShotDate().atZone(ZoneId.systemDefault()).toEpochSecond();
         long endEpoch = currentTarget.get().get(currentTarget.get().size() - 1).getSnapShotDate().atZone(ZoneId.systemDefault()).toEpochSecond();
         NumberAxis xAxis = (NumberAxis)topNChart.getXAxis();
-        xAxis.setTickUnit((endEpoch - startEpoch) / 100.0d);
+        xAxis.setTickUnit((endEpoch - startEpoch) / HeapStatsUtils.getXTickUnit());
         xAxis.setLowerBound(startEpoch);
         xAxis.setUpperBound(endEpoch);
         
