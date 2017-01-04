8033176126b5f6c2a4e26cec4474031cd710bc50
Yasumasa Suenaga <yasuenag@gmail.com>
2017-01-04 23:56:54 +0900

    Bug 3256: Some controls are overlapped when window is resized Reviewed-by: ykubota http://icedtea.classpath.org/bugzilla/show_bug.cgi?id=3256 Closes #53 #69

--- analyzer/fx/src/main/java/jp/co/ntt/oss/heapstats/plugin/builtin/snapshot/tabs/RefTreeController.java
+++ analyzer/fx/src/main/java/jp/co/ntt/oss/heapstats/plugin/builtin/snapshot/tabs/RefTreeController.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2014-2016 Yasumasa Suenaga
+ * Copyright (C) 2014-2017 Yasumasa Suenaga
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
@@ -17,6 +17,7 @@
  */
 package jp.co.ntt.oss.heapstats.plugin.builtin.snapshot.tabs;
 
+import javafx.scene.layout.VBox;
 import jp.co.ntt.oss.heapstats.snapshot.ReferenceTracker;
 import com.mxgraph.layout.hierarchical.mxHierarchicalLayout;
 import com.mxgraph.model.mxCell;
@@ -76,7 +77,7 @@ public class RefTreeController implements Initializable, MouseListener {
     private mxGraphComponent graphComponent;
 
     @FXML
-    private AnchorPane topAnchorPane;
+    private VBox topVBox;
 
     @FXML
     private Label snapshotLabel;
@@ -150,7 +151,7 @@ public class RefTreeController implements Initializable, MouseListener {
          * So I call repaint() method at last and request layout() call.
          */
         graphComponent.repaint();
-        Platform.runLater(() -> topAnchorPane.layout());
+        Platform.runLater(() -> topVBox.layout());
     }
 
     /**
@@ -267,7 +268,7 @@ public class RefTreeController implements Initializable, MouseListener {
         }
 
         graphComponent.repaint();
-        Platform.runLater(() -> topAnchorPane.layout());
+        Platform.runLater(() -> topVBox.layout());
     }
 
     @Override
