dd87a471cfb8244854f2ac7f97212db08fef07be
Yasumasa Suenaga <yasuenag@gmail.com>
2017-07-10 17:37:06 +0900

    Bug 3418: [TEST] GCWatcher and SnapShotProcessor tests might hang

--- agent/test/race-condition/GCWatcher/SnapShotProcessor/testcase/Test.java
+++ agent/test/race-condition/GCWatcher/SnapShotProcessor/testcase/Test.java
@@ -20,6 +20,7 @@ public class Test{
 
   public static void main(String[] args) throws Exception{
     System.gc();
+    Thread.sleep(3000); // Sleep 3 sec to wait breakpoint...
     System.gc();
   }
 
